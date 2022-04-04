2021c61af36ddc4139c00dad9f280634096454e1
SlawomirDudziak <50486478+SlawomirDudziak@users.noreply.github.com>
2022-04-04 21:59:45 +0200

    get and display values from html

--- index.php
+++ index.php
@@ -3,7 +3,45 @@
 include('simplehtmldom/simple_html_dom.php');
 
 $html = file_get_html('wo_for_parse.html');
+$new_line_char = '<br/>';
 
 $tracking_number = $html->find('#wo_number', 0)->plaintext;
+$po_number = $html->find('#po_number', 0)->plaintext;
+$customer = $html->find('#customer', 0)->plaintext;
+$trade = $html->find('#trade', 0)->plaintext;
 
-echo $tracking_number;
+$nte = $html->find('#nte', 0)->plaintext;
+$nte_number = preg_replace('/[$,]/', '', $nte);
+$nte_float = number_format(floatval($nte_number), 2, '.', '');
+
+$store_id = $html->find('#location_name', 0)->plaintext;
+
+$address_final = [];
+$address = $html->find('a#location_address', 0)->plaintext;
+$address_formatted = preg_replace('/\s+/', ',', $address);
+$address_array = explode(',', $address_formatted);
+foreach ($address_array as $address) {
+    if ($address != "") array_push($address_final, $address);
+}
+
+$street = implode(" ", array_slice($address_final, 0, 3));
+$city = $address_final[count($address_final)-3];
+$state = $address_final[count($address_final)-2];
+$post_code = $address_final[count($address_final)-1];
+
+$phone = $html->find('#location_phone', 0)->plaintext;
+$phone_number = preg_replace('/\D+/', '', $phone);
+$phone_float = floatval($phone_number);
+
+echo '<b>Tracking Number</b>: ' . $tracking_number . $new_line_char;
+echo '<b>PO Number</b>: ' . $po_number . $new_line_char;
+echo 'Data Scheduled w formacie daty i godziny (Y-m-d H:i): ' . $new_line_char;
+echo '<b>Customer</b>: ' . $customer . $new_line_char;
+echo '<b>Trade</b>: ' . $trade . $new_line_char;
+echo '<b>NTE (jako liczba float - bez formatowania)</b>: ' . $nte_float . $new_line_char;
+echo '<b>Store ID</b>: ' . $store_id . $new_line_char;
+echo '<b>Address z rozbiciem na ulica</b>: ' .  $street . $new_line_char;
+echo '<b>miasto</b>: ' . $city . $new_line_char;
+echo '<b>stan (2 litery)</b>: ' . $state . $new_line_char;
+echo '<b>kod pocztowy</b>: ' . $post_code . $new_line_char;
+echo '<b>Telefon (jako liczba float - bez formatowania)</b>: ' . $phone_float . $new_line_char;
